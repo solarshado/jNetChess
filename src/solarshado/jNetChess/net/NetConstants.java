4bc50ec16c88ab1ee6d559e3e2e3861a7f09506b
Adrian Todd <solarshado@gmail.com>
2011-05-05 17:27:45 -0400

    Added a couple constants for use by Lobby

--- src/solarshado/jNetChess/net/NetConstants.java
+++ src/solarshado/jNetChess/net/NetConstants.java
@@ -5,30 +5,30 @@ import java.net.*;
 public abstract class NetConstants {
 
     public static final int MULTICAST_PORT = 61020, TCP_PORT = 61020;
-    public static final InetAddress /*BROADCAST_ADDRESS, WILDCARD_ADDRESS,*/ MCAST_ADDRESS;
+    public static final InetAddress /* BROADCAST_ADDRESS, WILDCARD_ADDRESS, */MCAST_ADDRESS;
 
     static {
         InetAddress tmp = null;
         /*
-        try {
-            tmp = InetAddress.getByName("255.255.255.255");
-        }
-        catch (UnknownHostException e) {
-            // tmp = null //not good
-        }
-        finally {
-            BROADCAST_ADDRESS = tmp;
-        }
-        try {
-            tmp = InetAddress.getByName("0.0.0.0");
-        }
-        catch (UnknownHostException e) {
-            // tmp = null //not good
-        }
-        finally {
-            WILDCARD_ADDRESS = tmp;
-        }
-        */
+         * try {
+         * tmp = InetAddress.getByName("255.255.255.255");
+         * }
+         * catch (UnknownHostException e) {
+         * // tmp = null //not good
+         * }
+         * finally {
+         * BROADCAST_ADDRESS = tmp;
+         * }
+         * try {
+         * tmp = InetAddress.getByName("0.0.0.0");
+         * }
+         * catch (UnknownHostException e) {
+         * // tmp = null //not good
+         * }
+         * finally {
+         * WILDCARD_ADDRESS = tmp;
+         * }
+         */
         try {
             tmp = InetAddress.getByName("224.0.2.55");
         }
@@ -38,6 +38,10 @@ public abstract class NetConstants {
         finally {
             MCAST_ADDRESS = tmp;
         }
+    }
+
+    public static final class MulticastPacketType {
+        public static final byte NEW_GAME = 1, GAME_EXPIRED = 2;
     }
 
 }
