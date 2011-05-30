d6037f63be4bcb8055724e3e6394ba091a3a1a4e
Adrian Todd <solarshado@gmail.com>
2011-05-29 21:17:29 -0400

    New class for use by RemoteConnection

---
+++ src/solarshado/jNetChess/net/ConnectionCancledExcpetion.java
@@ -0,0 +1,15 @@
+package solarshado.jNetChess.net;
+
+/**
+ * @author Adrian Todd
+ *
+ */
+final class ConnectionCancledExcpetion extends Exception {
+
+    /**
+     * Thrown by {@link RemoteConnection}'s constructor when the connection is canceled by the user
+     */
+    public ConnectionCancledExcpetion() {
+        // TO-DO not... nothing _to_ do
+    }
+}
