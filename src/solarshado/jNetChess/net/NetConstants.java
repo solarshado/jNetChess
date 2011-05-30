65bf1186563edbfdc7a00dd5aedb2a2615053b25
Adrian Todd <solarshado@gmail.com>
2011-05-29 22:10:30 -0400

    Added 2 pre-converted byte[]s

--- src/solarshado/jNetChess/net/NetConstants.java
+++ src/solarshado/jNetChess/net/NetConstants.java
@@ -1,5 +1,6 @@
 package solarshado.jNetChess.net;
 
+import java.io.UnsupportedEncodingException;
 import java.net.*;
 
 public abstract class NetConstants {
@@ -13,14 +14,40 @@ public abstract class NetConstants {
             tmp = InetAddress.getByName("224.0.2.55");
         }
         catch (UnknownHostException e) {
-            // tmp = null //not good
+            // not good
+            throw new RuntimeException(e);
         }
         finally {
             MCAST_ADDRESS = tmp;
         }
     }
 
-    //TODO? replace w/ an Enum
+    static final byte[] ACCEPT_MSG, REJECT_MSG;
+    // damn ugly way to have to initialize the above vars...
+    static {
+        byte[] tmp = null;
+        try {
+            tmp = "accept".getBytes("ISO-8859-1");
+        }
+        catch (UnsupportedEncodingException e) {
+            assert false : "Character encoding 'ISO-8859-1' missing";
+        }
+        finally {
+            ACCEPT_MSG = tmp;
+        }
+
+        try {
+            tmp = "reject".getBytes("ISO-8859-1");
+        }
+        catch (UnsupportedEncodingException e) {
+            assert false : "Character encoding 'ISO-8859-1' missing";
+        }
+        finally {
+            REJECT_MSG = tmp;
+        }
+    }
+
+    // TODO? replace w/ an Enum
     public static final class MulticastPacketType {
         public static final byte NEW_GAME = 1, GAME_EXPIRED = 2;
     }
